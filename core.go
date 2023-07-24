9367203cdb7edb6f3ca340ece05ec58f097c5749
Tatsuo Ikeda / ikeikeikeike <jp.ne.co.jp@gmail.com>
2023-07-24 20:15:09 +0900

    packet rw

--- core.go
+++ core.go
@@ -141,3 +141,22 @@ const (
 var (
 	peerTable map[string]peer.ID
 )
+
+func run() {
+	h, err := genHost(port1)
+	if err != nil {
+		panic(err)
+	}
+	defer h.Close()
+
+	ifce, err := createInterface(name1, type1)
+	if err != nil {
+		panic(err)
+	}
+	defer ifce.Close()
+
+	h.SetStreamHandler(Protocol, streamHandler(ifce))
+
+	readPackets(context.Background(), h, ifce)
+
+}
--- main.go
+++ main.go
@@ -1,21 +1,5 @@
 package main
 
-import "context"
-
 func main() {
-	h, err := genHost(port1)
-	if err != nil {
-		panic(err)
-	}
-	defer h.Close()
-
-	ifce, err := createInterface(name1, type1)
-	if err != nil {
-		panic(err)
-	}
-	defer ifce.Close()
-
-	h.SetStreamHandler(Protocol, streamHandler(ifce))
-
-	readPackets(context.Background(), h, ifce)
+	run()
 }
