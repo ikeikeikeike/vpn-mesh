2a741907716cd1592e9b2dc36d1c0b4f4351fe81
Tatsuo Ikeda / ikeikeikeike <jp.ne.co.jp@gmail.com>
2023-08-09 20:01:38 +0900

    No panic should be product

--- CREDIT.md
+++ CREDIT.md
@@ -3,5 +3,3 @@
 - https://github.com/hyprspace/hyprspace
 - https://github.com/mudler/edgevpn
 - https://github.com/libp2p/go-libp2p
-
-
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"log"
 )
 
 func main() {
@@ -11,36 +12,36 @@ func main() {
 	// Arguments
 	args, err := parseArgs()
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 
 	// P2P Host
 	h, err := newP2P(args.Port)
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 	defer h.Close()
 
 	// Tun interface
 	i, err := newif(args.Interface)
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 	defer i.Close()
 	err = applyif(args.Interface, args.Network)
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 
 	// For local-network
 	dMDNS, err := newMDNS(h, args.Rendezvous)
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 	// For global-network
 	dDHT, err := newDHT(ctx, h, args.Rendezvous)
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 
 	fmt.Printf("Peer: %s\n", h.ID())
